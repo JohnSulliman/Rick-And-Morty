2b8a48cd953f036a63554bdf3059305e0becedc4
Sulliman <johnatantc@hotmail.com>
2021-06-11 00:25:33 -0300

    Adição de paginação no projeto Rick And Morty e mudanças feitas no CSS dos projetos Pokedex e Rick And Morty

--- aula12/rick-and-morty/src/Componentes/CardList.js
+++ aula12/rick-and-morty/src/Componentes/CardList.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
 import SearchBox from './SearchBox';
+import Footer from './Footer'
 
 class CardList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             isLoaded: false,
-            chars: []
+            pagina: 1,
+            chars: [],
         };
     }
 
-    API(nome) {
-        nome = nome ? nome: ''
-
-        fetch(`https://rickandmortyapi.com/api/character/?name=${nome}`)
+    API() {
+        fetch(`https://rickandmortyapi.com/api/character/?page=&name=`)
         .then(resultado => resultado.json())
         .then(resultadoJson =>{
             this.setState({
@@ -24,25 +24,44 @@ class CardList extends React.Component {
         });
     }
 
-    criarCardsChar() {
+    BuscarNome(evento) {
+        if (evento.key === 'Enter'){
+            const nome = evento.target.value
+
+            fetch(`https://rickandmortyapi.com/api/character/?page=&name=${nome}`)
+            .then(resultado => resultado.json())
+            .then(resultadoJson =>{
+                this.setState({
+                isLoaded: true,
+                chars: resultadoJson.results
+                })
+            });
+        }
+    }
+
+    LoadChars() {
+        const nextPage = this.state.pagina + 1;
+
+        fetch(`https://rickandmortyapi.com/api/character/?page=${nextPage}&nome=`)
+        .then(resultado => resultado.json())
+        .then(resultadoJson =>{
+            if(this.state.chars){
+                this.setState({
+                    isLoaded: true,
+                    pagina: nextPage,
+                    chars: this.state.chars.concat(resultadoJson.results),
+                })
+            }
+        });
+    }
+
+    createCardsChar() {
         const personagem = this.state.chars
         
         if(personagem) {
             return personagem.map((char) => {
                 return <Card char = {char} key = {char.id} />
             });
-        }
-        else {
-            <div className='body__card-list'>
-
-            </div>
-        }
-    }
-
-    Buscar(evento) {
-        if (evento.key === 'Enter'){
-            const nomePersonagem = evento.target.value
-            this.API(nomePersonagem)
         }
     }
 
@@ -59,16 +78,25 @@ class CardList extends React.Component {
         else {
             return(
                     <div className='body'>
-                        <div className='body__image'>
-                            <img src='https://imagensemoldes.com.br/wp-content/uploads/2021/04/Rick-and-Morty-PNG-1024x435.png' width='800' alt='Rick-And-Morty'/>
+                        <div className='body__background'>
+                            <img src='https://imagensemoldes.com.br/wp-content/uploads/2021/04/Rick-and-Morty-PNG-1024x435.png' 
+                            className='body__image'
+                            width='800' 
+                            alt='Rick-And-Morty'/>
                         </div>
                         <menu className='body__personagem'>
                             <span id='char'>Personagens</span>
                         </menu>
-                        <SearchBox placeholder='Buscar Personagens...' Buscar={(evento) => this.Buscar(evento)}/>
+                        <SearchBox placeholder='Buscar Personagens...' Buscar={(evento) => this.BuscarNome(evento)}/>
                         <div className='body__card-list'>
-                            {this.criarCardsChar()}
+                            {this.createCardsChar()}
                         </div>
+                        <div className='body__div-botao'>
+                            <button className='body__botao' onClick={() => {this.LoadChars()}}>
+                                Mais Personagens
+                            </button>
+                        </div>
+                        <Footer />
                     </div>
     
             );
